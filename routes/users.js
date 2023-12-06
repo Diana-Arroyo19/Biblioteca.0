580378630d569a9d3ab3d263cfc93b6e4ea1cc77
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2023-12-06 19:45:49 +0000

    🌠MEJORANDO APP

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+<<<<<<< HEAD
 module.exports = router;
+=======
+// GET /users/author
+router.get('/author', function(_, res) {
+  res.render('author', {author: "JOSE LUIS Y DIANA"});
+});
+
+
+module.exports = router;
+>>>>>>> 5d484f7 (🌠MEJORANDO APP)
