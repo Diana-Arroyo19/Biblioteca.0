2f78c67629a461485b463791c51aa74aad775c84
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2023-12-07 06:07:59 +0000

    MD

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,5 +1,5 @@
 import Debug from 'debug';
 // Establecemos el proceso a debuggear
-const debug = Debug('dwpcii:server');
+const debug = Debug('biblioteca.0:server');
 // exportando el logger de debuggeo
 export default debug;
