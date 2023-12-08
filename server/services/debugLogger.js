c2369ee6bf9e42ccc00cc3c8ed67675e33d61598
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2023-12-08 08:52:31 +0000

    Berificacion de errores

--- server/services/debugLogger.js
+++ server/services/debugLogger.js
@@ -1,6 +1,6 @@
 // Importando dependencia debug
 import Debug from 'debug';
 // Establecemos el proceso a debuggear
-const debug = Debug('Biblioteca.0');
+const debug = Debug('biblioteca.0');
 // exportando el logger de debuggeo
 export default debug;
