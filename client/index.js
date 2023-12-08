9d2848e74a073fdf2fb8c7d1176fb619e009dcb5
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2023-12-08 04:46:39 +0000

    ⭐listado y eliminación de libros

--- client/index.js
+++ client/index.js
@@ -4,8 +4,17 @@ import './styles/style.css';
 import 'materialize-css/dist/css/materialize.css';
 // Importando scripts de Materialize
 import 'materialize-css/dist/js/materialize';
+// Script para borrar proyecto
+import deleteBook from './domains/book.dashboard';
 
 // Inicializando Scripts de Materialize para interactividad
 M.AutoInit();
+
+// Cargando script en caso de que la URL sea '/book'
+if (window.location.pathname === '/book') {
+  console.log('Script cargado correctamente');
+  window.deleteBook = deleteBook;
+}
+
 // Mensaje en la consola
 console.log('🎉 Estilos cargados correctamente 🎉');
