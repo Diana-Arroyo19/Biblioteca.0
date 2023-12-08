f70e78c3a8b87b3c508ab4613c003b199fb4335a
LUISREYESELTRENZAS <124694031+LUISREYESELTRENZAS@users.noreply.github.com>
2023-12-08 06:01:32 +0000

    libros nuevos

--- server/domains/book/book.controller.js
+++ server/domains/book/book.controller.js
@@ -56,3 +56,25 @@ const addBook = async (req, res) => {
     return res.status(500).json(error);
   }
 };
+
+// DELETE "/project/:id"
+const deleteBook = async (req, res) => {
+  // Extrayendo el id de los parametros
+  const { id } = req.params;
+  // Usando el modelo para borrar el proyecto
+  try {
+    const result = await BookModel.findByIdAndRemove(id);
+    // Agregando mensaje de flash
+    req.flash('successMessage', 'Proyecto borrado con exito');
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+// Controlador Home
+export default {
+  showDashboard,
+  addForm,
+  addBook,
+  deleteBook,
+};
