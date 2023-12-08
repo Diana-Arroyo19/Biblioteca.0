40a924f36cf473f8d71de436242e8e6ed32649be
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2023-12-08 05:31:44 +0000

    ⭐Mejora: Validar con Yup

--- server/domains/book/book.router.js
+++ server/domains/book/book.router.js
@@ -1,14 +1,18 @@
 // Importando el Router de Express
 import { Router } from 'express';
-
 // Importando el controlador
 import bookController from './book.controller';
+
+// Importando factory de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando el validador de proyectos
+import bookValidator from './book.validator';
 
 // Creando una isntancia del enrutador
 const router = new Router();
 
 // Enrutamos
-// GET '//'
+// GET '/book/'
 // GET '/book/dashboard'
 router.get(['/', '/dashboard'], bookController.showDashboard);
 // GET '/book/add-form'
@@ -18,6 +22,14 @@ router.get(['/add-form', '/add'], bookController.addForm);
 router.delete('/:id', bookController.deleteBook);
 // POST "/project/add"
 router.post('/add', bookController.addBook);
+router.post(
+  '/add',
+  ValidateFactory({
+    schema: bookValidator.bookSchema,
+    getObject: bookValidator.getBook,
+  }),
+  bookController.addBook,
+);
 
 // Exporto este tramo de ruta
 export default router;
